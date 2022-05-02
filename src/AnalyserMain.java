98e88e8f43aaf769ec9b803bf78cd194ea569ae8
classroom <himanshukanoje23@gmail.com>
2022-05-02 08:49:28 +0530

    my commit

---
+++ src/AnalyserMain.java
@@ -0,0 +1,10 @@
+
+
+	package com.bridgelabz.assignment;
+
+	public class AnalyserMain {
+	    public static void main(String[] args) {
+	        System.out.println("Welcome In Indian States  Census Analyser Program !!!");
+	    }
+	}
+}
