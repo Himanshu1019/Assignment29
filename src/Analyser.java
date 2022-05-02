ba3d8e808574cc77b4a14f519e87184ff65c72fa
classroom <himanshukanoje23@gmail.com>
2022-05-02 08:42:04 +0530

    my commit

---
+++ src/Analyser.java
@@ -0,0 +1,16 @@
+
+
+
+	public class Analyser extends Exception {
+	    enum ExceptionType {
+	        CENSUS_FILE_PROBLEM, CSV_FILE_INTERNAL_ISSUES, INCORRECT_FILE_FORMAT, NO_CENSUS_DATA
+	    }
+
+	    ExceptionType type;
+
+	    public CensusAnalyserException(String message, ExceptionType type) {
+	        super(message);
+	        this.type = type;
+	    }
+	}
+
